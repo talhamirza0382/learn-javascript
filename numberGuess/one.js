58be66201614bf6e6b00a4ba0901082c3c343a5a
Mirza <talhamirza0382@gmail.com>
2020-03-27 19:20:32 +0530

    ww

--- numberGuess/one.js
+++ numberGuess/one.js
@@ -1,2 +1,15 @@
 // Math.floor( Math.random()* 20 )+1
-// will generate no b/w 1 to 20, 20 also
+// will generate no 1, 20 and between them.
+
+var numBer=()=>{
+    var z= Math.floor( Math.random()* 20 )+1
+    return z
+}
+
+var guessCount=1
+var guessbox = document.querySelector('.guessField');
+var guessbox2 = document.querySelector('.guessField2');
+var guessbox3 = document.querySelector('.guessField3');
+guessbox.focus();
+guessbox2.focus();
+guessbox3.focus();
